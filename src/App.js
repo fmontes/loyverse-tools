1b33c50db80b678b73ada062cf70c0df2038d73d
Freddy Montes <freddymontes@gmail.com>
2020-01-01 22:39:29 -0600

    Load products, display and sort

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Table from './components/Table';
+
 import './App.css';
 
+function parseData(csv) {
+    var lines = csv.split('\n');
+
+    var result = [];
+    var headers = lines[0].split(',');
+
+    for (var i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentline = lines[i].split(',');
+
+        for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+
+        result.push(obj);
+    }
+
+    return result; //JavaScript object
+}
+
+function work(target) {
+    return new Promise((resolve, reject) => {
+        const file = target.files[0];
+
+        var reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+        reader.onload = ({ target }) => {
+            const data = parseData(target.result);
+            const res = data
+                .filter(item => {
+                    return item['Disponibles para la venta [Smile]'] === 'Y';
+                })
+                .map(item => {
+                    return {
+                        name: item['Nombre'],
+                        qty: parseInt(item['En inventario [Smile]']),
+                        supplier: item['Proveedor']
+                    };
+                })
+                .sort(function(itemA, itemB) {
+                    return itemA.qty - itemB.qty;
+                });
+            return resolve(res);
+        };
+        reader.onerror = () => {
+            reject('Error');
+        };
+    });
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const columns = React.useMemo(
+        () => [
+            {
+                Header: 'Nombre',
+                accessor: 'name'
+            },
+            {
+                Header: 'Cantidad',
+                accessor: 'qty'
+            },
+            {
+                Header: 'Proveedor',
+                accessor: 'supplier'
+            }
+        ],
+        []
+    );
+
+    const [data, setData] = useState([]);
+    return (
+        <div className="App">
+            <h1>Productos con inventario bajo</h1>
+            <input
+                type="file"
+                onChange={async ({ currentTarget }) => {
+                    const data = await work(currentTarget);
+                    setData(data);
+                }}
+            />
+            <hr />
+            {data.length ? <Table columns={columns} data={data} /> : null}
+        </div>
+    );
 }
 
 export default App;
