796681c903c57b7210f95eb76ced046ad4219e90
Freddy Montes <freddymontes@gmail.com>
2020-01-01 22:42:45 -0600

    Updates

---
+++ precache-manifest.fb90c8b279be3ce6c6462e834d531b3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854f51d02f2f882d43e9ed37ffe41b1c",
+    "url": "/loyverse-tools/index.html"
+  },
+  {
+    "revision": "7ba01d5a36c91f691724",
+    "url": "/loyverse-tools/static/css/main.33c57d20.chunk.css"
+  },
+  {
+    "revision": "45b0861e4784597eb57c",
+    "url": "/loyverse-tools/static/js/2.e0d3d9a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/loyverse-tools/static/js/2.e0d3d9a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ba01d5a36c91f691724",
+    "url": "/loyverse-tools/static/js/main.c44d814b.chunk.js"
+  },
+  {
+    "revision": "def85d45ff282e88f004",
+    "url": "/loyverse-tools/static/js/runtime-main.11fd4600.js"
+  }
+]);
